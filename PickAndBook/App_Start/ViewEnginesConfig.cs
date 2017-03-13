cf509b6759cbaf892480aa46243c867a95513ea3
Strateva <MiroslavaStrateva@gmail.com>
2017-03-14 01:37:44 +0200

    Add project files.

---
+++ PickAndBook/App_Start/ViewEnginesConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace PickAndBook
+{
+    public class ViewEnginesConfig
+    {
+        public static void RegisterViewEngines()
+        {
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
+        }
+    }
+}
