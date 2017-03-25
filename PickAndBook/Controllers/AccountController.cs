fe0ae59d91677508747d5b51993f242c3938b06b
Strateva <MiroslavaStrateva@gmail.com>
2017-03-25 18:53:55 +0200

    Assign Client role to newly registered users

--- PickAndBook/Controllers/AccountController.cs
+++ PickAndBook/Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using PickAndBook.Models;
 using PickAndBook.Auth;
+using PickAndBook.Data.Common;
 
 namespace PickAndBook.Controllers
 {
@@ -153,15 +154,20 @@ namespace PickAndBook.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
-                    // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
-                    // Send an email with this link
-                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+                    var roleResult = await UserManager.AddToRoleAsync(user.Id, DataConstants.ClientRoleName);
+                    if (roleResult.Succeeded)
+                    {
+                        await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
 
-                    return RedirectToAction("Index", "Home");
+                        // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
+                        // Send an email with this link
+                        // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                        // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                        // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+                        return RedirectToAction("Index", "Home");
+                    }
+                    AddErrors(roleResult);
                 }
                 AddErrors(result);
             }
---
+++ Tests/PickAndBook.Tests/Controllers/AccountControllerTests/Register_Should.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using Moq;
+using NUnit.Framework;
+using PickAndBook.Auth;
+using PickAndBook.Controllers;
+using PickAndBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using TestStack.FluentMVCTesting;
+
+namespace PickAndBook.Tests.Controllers.AccountControllerTests
+{
+    [TestFixture]
+    public class Register_Should
+    {
+        [Test]
+        public void RedirectToHomeIndex_WhenUserRegisteredAndGivenRoleClient()
+        {
+            // Arrange
+            var viewModel = new RegisterViewModel();
+            var userStoreMock = new Mock<IUserStore<ApplicationUser>>();
+            var userManagerMock = new Mock<ApplicationUserManager>(userStoreMock.Object);
+            var authenticationManagerMock = new Mock<IAuthenticationManager>();
+            var signInManagerMock = new Mock<ApplicationSignInManager>(userManagerMock.Object, authenticationManagerMock.Object);
+            
+            userManagerMock.Setup(m => m.CreateAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>()))
+                       .Returns(Task.FromResult(IdentityResult.Success));
+            userManagerMock.Setup(m => m.AddToRoleAsync(It.IsAny<string>(), It.IsAny<string>()))
+                       .Returns(Task.FromResult(IdentityResult.Success));
+            var accountController = new AccountController(userManagerMock.Object, signInManagerMock.Object);
+
+            // Act & Assert
+            accountController.WithCallTo(c => c.Register(viewModel))
+                .ShouldRedirectTo<HomeController>(c2 => c2.Index());
+        }
+
+        [Test]
+        public void ReturnDefaultView_WhenUserRegisterIsNotSuccessful()
+        {
+            // Arrange
+            var viewModel = new RegisterViewModel();
+            var userStoreMock = new Mock<IUserStore<ApplicationUser>>();
+            var userManagerMock = new Mock<ApplicationUserManager>(userStoreMock.Object);
+            var authenticationManagerMock = new Mock<IAuthenticationManager>();
+            var signInManagerMock = new Mock<ApplicationSignInManager>(userManagerMock.Object, authenticationManagerMock.Object);
+
+            userManagerMock.Setup(m => m.CreateAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>()))
+                       .Returns(Task.FromResult(IdentityResult.Success));
+            userManagerMock.Setup(m => m.AddToRoleAsync(It.IsAny<string>(), It.IsAny<string>()))
+                       .Returns(Task.FromResult(IdentityResult.Failed(new string[] { "Error 1", "Error 2" })));
+
+            var accountController = new AccountController(userManagerMock.Object, signInManagerMock.Object);
+            var expectedViewModel = new RegisterViewModel();
+
+            // Act & Assert
+            accountController.WithCallTo(c => c.Register(expectedViewModel))
+                .ShouldRenderDefaultView()
+                .WithModel(expectedViewModel);
+        }
+
+        [Test]
+        public void ReturnDefaultView_WhenUserClientRoleAssignmentIsNotSuccessful()
+        {
+            // Arrange
+            var viewModel = new RegisterViewModel();
+            var userStoreMock = new Mock<IUserStore<ApplicationUser>>();
+            var userManagerMock = new Mock<ApplicationUserManager>(userStoreMock.Object);
+            var authenticationManagerMock = new Mock<IAuthenticationManager>();
+            var signInManagerMock = new Mock<ApplicationSignInManager>(userManagerMock.Object, authenticationManagerMock.Object);
+
+            userManagerMock.Setup(m => m.CreateAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>()))
+                       .Returns(Task.FromResult(IdentityResult.Failed(new string[] { "AA", "BB" })));
+
+            var accountController = new AccountController(userManagerMock.Object, signInManagerMock.Object);
+            var expectedViewModel = new RegisterViewModel();
+
+            // Act & Assert
+            accountController.WithCallTo(c => c.Register(expectedViewModel))
+                .ShouldRenderDefaultView()
+                .WithModel(expectedViewModel);
+        }
+
+        [Test]
+        public void ReturnDefaultView_WhenModelStateIsInvalid()
+        {
+            // Arrange
+            var accountController = new AccountController();
+            var expected = new RegisterViewModel();
+            accountController.ModelState.AddModelError("Error", "Test Error");
+
+            // Act & Assert
+            accountController.WithCallTo(c => c.Register(expected))
+                .ShouldRenderDefaultView()
+                .WithModel(expected);
+        }
+    }
+}
